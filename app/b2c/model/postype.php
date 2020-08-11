2b4c7b32666b9b529ada8fd78fe1290016fc6f4d
613554006@qq.com <613554006@qq.com>
2020-08-11 13:25:17 +0800

    刷卡方式更新

--- app/b2c/model/postype.php
+++ app/b2c/model/postype.php
@@ -6,41 +6,44 @@
  * @license  http://ecos.shopex.cn/ ShopEx License
  */
 
-
 /**
  * brand 模板
  */
-class b2c_mdl_postype extends dbeav_model{
-    var $defaultOrder = array('posbrand_id',' ASC');
-    public function getPostype(){
-    	//刷卡方式
+class b2c_mdl_postype extends dbeav_model {
+    public $defaultOrder = array('posbrand_id', ' ASC');
+    public function getPostype() {
+        //刷卡方式
         $postype = array(
-            'da_e'=>'大额',
-            'hui_ka'=>'挥卡',
-            'weixin'=>'微信扫码',
-            'zhifubao'=>'支付宝扫码',
-            'mobile_pay'=>'手机pay',
-            'jd'=>'京东扫码',
-            'bank_app'=>'银行app扫码',
-            'yun_pay'=>'银联云闪付',
-            'hongshan'=>'红闪封顶',
-            'long_pay'=>'龙支付',
-            'qianbao'=>'钱宝app',
-            'heika1'=>'常州·餐饮',
-            'heika2'=>'常州·百货',
-            'heika3'=>'无锡·加油站',
-            'heika4'=>'无锡·酒吧',
-            'heika5'=>'苏州·旅行社',
+            'da_e'       => '大额',
+            'hui_ka'     => '挥卡',
+            'weixin'     => '微信扫码',
+            'zhifubao'   => '支付宝扫码',
+            'mobile_pay' => '手机pay',
+            'jd'         => '京东扫码',
+            'bank_app'   => '银行app扫码',
+            'yun_pay'    => '银联云闪付',
+            'hongshan'   => '红闪封顶',
+            'long_pay'   => '龙支付',
+            'qianbao'    => '钱宝app',
+            'heika1'     => '常州·餐饮',
+            'heika2'     => '常州·百货',
+            'heika3'     => '无锡·加油站',
+            'heika4'     => '无锡·酒吧',
+            'heika5'     => '苏州·旅行社',
+
+            'heika6'     => '南京·酒店',
+            'heika7'     => '南京·旅行社',
+            'heika8'     => '常州·酒吧',
         );
         return $postype;
     }
 
-    public function getList($cols='*', $filter=array(), $offset=0, $limit=-1, $orderType=null){
-        $datas = parent::getList($cols, $filter, $offset, $limit, $orderType);
+    public function getList($cols = '*', $filter = array(), $offset = 0, $limit = -1, $orderType = null) {
+        $datas   = parent::getList($cols, $filter, $offset, $limit, $orderType);
         $postype = $this->getPostype();
         foreach ($datas as $key => &$value) {
             $value['shuaka_typeOld'] = $value['shuaka_type'];
-            $value['shuaka_type'] = $postype[$value['shuaka_type']];
+            $value['shuaka_type']    = $postype[$value['shuaka_type']];
         }
         return $datas;
     }
